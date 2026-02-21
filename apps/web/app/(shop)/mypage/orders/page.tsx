78cbf5733cc316098ecd5587435cc10d3587e908
wooceo <wooceo@gmail.com>
2026-02-22 01:18:29 +0900

    auto: 20260222 01:18

--- apps/web/app/(shop)/mypage/orders/page.tsx
+++ apps/web/app/(shop)/mypage/orders/page.tsx
@@ -463,7 +463,7 @@ export default function MyOrdersPage() {
                         aria-label="전체 선택"
                       />
                     </TableHead>
-                    <TableHead className="text-center w-[150px] text-xs">주문일<br />(주문번호)</TableHead>
+                    <TableHead className="text-center w-[300px] text-xs">주문일<br />(주문번호)</TableHead>
                     <TableHead className="text-xs">상품명<br />규격</TableHead>
                     <TableHead className="text-center w-[70px] text-xs">페이지<br />/ 부수</TableHead>
                     <TableHead className="text-center w-[65px] text-xs">용량</TableHead>
