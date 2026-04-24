4530e2cce950ecd9a6f3dc39fba4f78f5b352759
woo youngkug <your@email.comwooceo@gmail.com>
2026-04-24 16:41:34 +0900

    feat: originals Private 버킷 스토리지 서비스 추가

--- apps/api/src/modules/upload/services/originals-storage.service.ts
+++ apps/api/src/modules/upload/services/originals-storage.service.ts
@@ -1,52 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import {
-    S3Client,
-    PutObjectCommand,
-    DeleteObjectCommand,
-    GetObjectCommand,
-} from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand, DeleteObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 @Injectable()
 export class OriginalsStorageService {
-    private readonly s3: S3Client;
-    private readonly bucket: string;
+  private readonly s3: S3Client;
+  private readonly bucket: string;
 
-    constructor(private readonly config: ConfigService) {
-        this.bucket = config.get<string>('B2_ORIGINALS_BUCKET', '');
-        this.s3 = new S3Client({
-            region: 'auto',
-            endpoint: config.get<string>('B2_ORIGINALS_ENDPOINT'),
-            credentials: {
-                accessKeyId: config.get<string>('B2_ORIGINALS_KEY_ID', ''),
-                secretAccessKey: config.get<string>('B2_ORIGINALS_APP_KEY', ''),
-            },
-        });
-    }
+  constructor(private config: ConfigService) {
+    this.bucket = config.getOrThrow<string>('B2_ORIGINALS_BUCKET');
+    this.s3 = new S3Client({
+      region: 'us-east-005',
+      endpoint: config.getOrThrow<string>('B2_ORIGINALS_ENDPOINT'),
+      credentials: {
+        accessKeyId: config.getOrThrow<string>('B2_ORIGINALS_KEY_ID'),
+        secretAccessKey: config.getOrThrow<string>('B2_ORIGINALS_APP_KEY'),
+      },
+    });
+  }
 
-    async upload(buffer: Buffer, key: string, mimetype: string): Promise<void> {
-        await this.s3.send(
-            new PutObjectCommand({
-                Bucket: this.bucket,
-                Key: key,
-                Body: buffer,
-                ContentType: mimetype,
-            }),
-        );
-    }
+  async upload(buffer: Buffer, key: string, mimetype: string): Promise<string> {
+    await this.s3.send(new PutObjectCommand({
+      Bucket: this.bucket,
+      Key: key,
+      Body: buffer,
+      ContentType: mimetype,
+    }));
+    return key;
+  }
 
-    async getPresignedUrl(key: string, expiresIn = 3600): Promise<string> {
-        return getSignedUrl(
-            this.s3,
-            new GetObjectCommand({ Bucket: this.bucket, Key: key }),
-            { expiresIn },
-        );
-    }
+  async getPresignedUrl(key: string, expiresIn = 3600): Promise<string> {
+    return getSignedUrl(
+      this.s3,
+      new GetObjectCommand({ Bucket: this.bucket, Key: key }),
+      { expiresIn },
+    );
+  }
 
-    async delete(key: string): Promise<void> {
-        await this.s3.send(
-            new DeleteObjectCommand({ Bucket: this.bucket, Key: key }),
-        );
-    }
+  async delete(key: string): Promise<void> {
+    await this.s3.send(new DeleteObjectCommand({
+      Bucket: this.bucket,
+      Key: key,
+    }));
+  }
 }
